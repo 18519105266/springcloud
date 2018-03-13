990d9a93bf7fb9823e2a59e292bbf17fc2178088
Yanb1128 <15600100928@163.com>
2018-03-13 17:12:35 +0800

    增加eureka server

--- cloud-114/src/main/java/org/soft/cloud/Cloud114App.java
+++ cloud-114/src/main/java/org/soft/cloud/Cloud114App.java
@@ -1,10 +1,19 @@
 package org.soft.cloud;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
 public class Cloud114App {
+	private static final Logger logger = LoggerFactory.getLogger(Cloud114App.class);
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(2);
+		SpringApplication.run(Cloud114App.class, args);
+		logger.info("114 服务平台已启动~~~"); 
 	}
 
 }
